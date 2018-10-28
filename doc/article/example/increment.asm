9cc80c046ab0085c69480e6c18e584ff6cf059ac
Лев Воробьев <lev.vorobjev@rambler.ru>
2018-10-28 14:38:32 +0300

    Init documents

---
+++ doc/article/example/increment.asm
@@ -0,0 +1,10 @@
+     cmp  al, '1'
+     jne  .e1
+     mov  eax, 00320000h
+     ret
+.e1: cmp  al, 0
+     jne  .e2
+     mov  eax, 0FF200031h
+     ret
+.e2: mov  eax,00800000h
+     ret
---
+++ doc/presentation/example/increment.c
@@ -0,0 +1,8 @@
+switch(al) {
+case '1':
+	return 0x00320000;
+case 0:
+	return 0xFF200031;
+default:
+	return 0x00800000;
+};
